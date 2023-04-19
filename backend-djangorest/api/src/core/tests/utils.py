bc95b3e0e0b489f6cc349502ee180366d5aa08bb
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-04-19 21:25:13 +0200

    Dejangorest/api key tests (#19) (#20)
    * Tests finished
    
    * Update docker-compose.yml

--- backend-djangorest/api/src/core/tests/utils.py
+++ backend-djangorest/api/src/core/tests/utils.py
@@ -40,47 +40,8 @@ def patch(client: APIClient, url: str, data: dict, content_type="application/jso
     )
 
 
-def patch_image(client: APIClient, url: str, data: dict):
-    return client.patch(
-        url,
-        data=data,
-    )
-
-
 def delete(client: APIClient, url: str, content_type="application/json"):
     return client.delete(
         url,
         content_type=content_type
     )
-
-
-def authenticate_user(client: APIClient, credentials: dict):
-    jwt_obtain_url = reverse('jwt_obtain_pair')
-    # Get jwt token
-    response = post(client, jwt_obtain_url, credentials)
-    try:
-        jwt = response.data['access']
-        client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(jwt))
-    except:
-        pass
-    return response
-
-
-def get_access_token(client: APIClient, credentials: dict):
-    jwt_obtain_url = reverse('jwt_obtain_pair')
-    return post(client, jwt_obtain_url, credentials).data['access']
-
-
-def get_refresh_token(client: APIClient, credentials: dict):
-    jwt_obtain_url = reverse('jwt_obtain_pair')
-    return post(client, jwt_obtain_url, credentials).data['refresh']
-
-
-def access_token(client: APIClient, credentials: dict):
-    jwt_obtain_url = reverse('jwt_obtain_pair')
-    return post(client, jwt_obtain_url, credentials)
-
-
-def refresh_token(client: APIClient, token: str):
-    jwt_refresh_url = reverse('jwt_refresh')
-    return post(client, jwt_refresh_url, {'refresh': token})
