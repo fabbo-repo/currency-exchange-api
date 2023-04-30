2311cfb1f36713a70e89e2b6a6f042d5d2852cb8
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-04-30 14:45:07 +0200

    Update exceptions.py (#30)

--- backend-djangorest/api/src/core/exceptions.py
+++ backend-djangorest/api/src/core/exceptions.py
@@ -1,12 +1,28 @@
 from rest_framework.views import exception_handler
 from rest_framework import status
 from rest_framework.exceptions import APIException
+from rest_framework.serializers import ValidationError
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def app_exception_handler(exc, context):
+def app_exception_handler(exc: Exception, context):
     response = exception_handler(exc, context)
     if isinstance(exc, AppBadRequestException):
         response.data['error_code'] = exc.error_code
+    elif isinstance(exc, ValidationError):
+        fields = []
+        for field_name in exc.detail:
+            for detail in exc.detail[field_name]:
+                fields.append({
+                    "name": field_name,
+                    "detail": detail
+                })
+        response.data = {
+            'fields': fields,
+            "detail": exc.default_detail
+        }
     return response
 
 
