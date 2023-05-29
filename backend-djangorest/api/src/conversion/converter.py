9c2b6a77c64075a3204b8407a8766da89c938b83
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-05-29 20:57:58 +0200

    Djangorest/conversion fix (#33)
    * Update Readme
    
    * Update version

--- backend-djangorest/api/src/conversion/converter.py
+++ backend-djangorest/api/src/conversion/converter.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from conversion.models import Conversion
 from currency.models import Currency
 from django.utils import timezone
+from time import sleep
 
 logger = logging.getLogger(__name__)
 
@@ -12,44 +13,49 @@ CURRENCY_CONVERTER_URL = "https://www.xe.com/currencyconverter/convert/"
 
 
 class CurrencyConversionService:
-
     def make_conversions(reference_currency_code, ammount=1) -> dict:
-        res = {
-            reference_currency_code: ammount
-        }
+        res = {reference_currency_code: ammount}
         for code in settings.CURRENCY_CODES:
             # Ignore reference_currency_code
             if code == reference_currency_code:
                 continue
             res[code] = CurrencyConversionService.make_conversion(
-                reference_currency_code, code, ammount)
+                reference_currency_code, code, ammount
+            )
+            # Wait half second for next conversion lookup
+            sleep(0.5)
         return res
 
     def make_conversion(currency_from: str, currency_to: str, ammount=1) -> float:
-        if currency_from == currency_to:
+        code_from = min(currency_from, currency_to)
+        code_to = max(currency_from, currency_to)
+        if code_from == code_to:
             return ammount
-        last_conversion = Conversion.objects.filter(
-            currency_from=currency_from,
-            currency_to=currency_to
+        now = timezone.now()
+        conversions = Conversion.objects.filter(
+            currency_from=code_from,
+            currency_to=code_to,
+            created_at__gte=now
+            - timezone.timedelta(minutes=settings.MAX_NO_UPDATED_MINS),
         )
-        if last_conversion.exists():
-            last_conversion = last_conversion.last()
-            if last_conversion.created_at > timezone.now() - timezone.timedelta(minutes=settings.MAX_NO_UPDATED_MINS):
-                conversion_value = last_conversion.conversion_value
-                if last_conversion.created_at < timezone.now() - timezone.timedelta(days=settings.MAX_STORED_DAYS):
-                    last_conversion.delete()
-                return conversion_value * ammount
-            else:
-                if last_conversion.created_at < timezone.now() - timezone.timedelta(days=settings.MAX_STORED_DAYS):
-                    last_conversion.delete()
+        if conversions.exists():
+            last_conversion = conversions.last()
+            conversion_value = last_conversion.conversion_value
+            return conversion_value * ammount
         requestd_conersion = CurrencyConversionService._request_conversion(
-            currency_from, currency_to, 1)
+            code_from, code_to, 1
+        )
         Conversion.objects.create(
-            currency_from=Currency.objects.get(code=currency_from),
-            currency_to=Currency.objects.get(code=currency_to),
-            conversion_value=requestd_conersion
+            currency_from=Currency.objects.get(code=code_from),
+            currency_to=Currency.objects.get(code=code_to),
+            conversion_value=requestd_conersion,
         )
         return requestd_conersion * ammount
+
+    def delete_conversions():
+        limit_date = timezone.now() - timezone.timedelta(days=settings.MAX_STORED_DAYS)
+        currency_conversions = Conversion.objects.filter(created_at__lt=limit_date)
+        currency_conversions.delete()
 
     def _request_conversion(currency_from, currency_to, ammount=1) -> float:
         params = {}
@@ -60,16 +66,15 @@ class CurrencyConversionService:
         try:
             resp.raise_for_status()
         except:
-            logger.error(
-                'Currency converter connection error')
+            logger.error("Currency converter connection error")
             raise ConnectionError()
         html = resp.content
         return CurrencyConversionService._filter_html_response(html)
 
     def _filter_html_response(html) -> float:
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(html, "html.parser")
         try:
-            return float(soup.body.find_all('p')[3].text.split(' ')[3])
+            return float(soup.body.find_all("p")[3].text.split(" ")[3])
         except:
-            logger.error('Html parser no longer valid')
-            raise Exception('html parser no longer valid')
+            logger.error("Html parser no longer valid")
+            raise Exception("html parser no longer valid")
