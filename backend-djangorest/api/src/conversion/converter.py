17949f5eb5dd44f09d5a57ccdf4aa0aea04feec5
Fabián Scherle <47057180+fabbo-repo@users.noreply.github.com>
2023-06-01 18:59:26 +0200

    Converter hotfix

--- backend-djangorest/api/src/conversion/converter.py
+++ backend-djangorest/api/src/conversion/converter.py
@@ -13,7 +13,7 @@ CURRENCY_CONVERTER_URL = "https://www.xe.com/currencyconverter/convert/"
 
 
 class CurrencyConversionService:
-    def make_conversions(reference_currency_code, ammount=1) -> dict:
+    def make_conversions(reference_currency_code, ammount=1, is_job=False) -> dict:
         res = {reference_currency_code: ammount}
         for code in settings.CURRENCY_CODES:
             # Ignore reference_currency_code
@@ -22,19 +22,18 @@ class CurrencyConversionService:
             res[code] = CurrencyConversionService.make_conversion(
                 reference_currency_code, code, ammount
             )
-            # Wait half second for next conversion lookup
-            sleep(0.5)
+            if is_job:
+                # Wait half second for next conversion lookup
+                sleep(0.5)
         return res
 
     def make_conversion(currency_from: str, currency_to: str, ammount=1) -> float:
-        code_from = min(currency_from, currency_to)
-        code_to = max(currency_from, currency_to)
-        if code_from == code_to:
+        if currency_from == currency_to:
             return ammount
         now = timezone.now()
         conversions = Conversion.objects.filter(
-            currency_from=code_from,
-            currency_to=code_to,
+            currency_from=currency_from,
+            currency_to=currency_to,
             created_at__gte=now
             - timezone.timedelta(minutes=settings.MAX_NO_UPDATED_MINS),
         )
@@ -43,18 +42,19 @@ class CurrencyConversionService:
             conversion_value = last_conversion.conversion_value
             return conversion_value * ammount
         requestd_conersion = CurrencyConversionService._request_conversion(
-            code_from, code_to, 1
+            currency_from, currency_to, 1
         )
         Conversion.objects.create(
-            currency_from=Currency.objects.get(code=code_from),
-            currency_to=Currency.objects.get(code=code_to),
+            currency_from=Currency.objects.get(code=currency_from),
+            currency_to=Currency.objects.get(code=currency_to),
             conversion_value=requestd_conersion,
         )
         return requestd_conersion * ammount
 
     def delete_conversions():
         limit_date = timezone.now() - timezone.timedelta(days=settings.MAX_STORED_DAYS)
-        currency_conversions = Conversion.objects.filter(created_at__lt=limit_date)
+        currency_conversions = Conversion.objects.filter(
+            created_at__lt=limit_date)
         currency_conversions.delete()
 
     def _request_conversion(currency_from, currency_to, ammount=1) -> float:
@@ -74,7 +74,7 @@ class CurrencyConversionService:
     def _filter_html_response(html) -> float:
         soup = BeautifulSoup(html, "html.parser")
         try:
-            return float(soup.body.find_all("p")[3].text.split(" ")[3])
+            return float(soup.body.find_all("p")[2].text.split(" ")[0])
         except:
             logger.error("Html parser no longer valid")
             raise Exception("html parser no longer valid")
